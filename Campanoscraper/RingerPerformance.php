56dd7e64430fc92146853fb3511c52cbdfb0e524
Thomas Wood <grand.edgemaster@gmail.com>
2010-08-10 13:37:40 +0100

    Nicer ringer detail parsing

--- Campanoscraper/RingerPerformance.php
+++ Campanoscraper/RingerPerformance.php
@@ -13,27 +13,25 @@ class RingerPerformance extends DatabaseRecord {
 
     $this->bell = (int) $bell;
 
-    $name = utf8_decode($str);
-    $this->credit = $name;
+    $str = utf8_decode($str);
 
-    // Check for conductor, and strip (C... if found
-    if(($end = stripos($str, '(c')) !== false) {
-      $name = $this->mytrim(substr($str, 0, $end));
-      $this->conductor = true;
-    } else {
-      $name = $this->mytrim($str);
-    }
+    // Strip conductor tag, set flag using the replacement count (max 1)
+    $str = preg_replace( '|\(C(ond.?(uctor)?)? ?\)|i', '', $str, 1, $this->conductor);
 
-    // Check for further in-name footnotes (will not find additional ones after (c))
-    if(($footnote = strstr($name, '(')) !== false) {
-      $this->footnote = $this->mytrim($footnote, '()');
-      $name = $this->mytrim(str_replace($footnote, '', $name));
-    }
+    // Find other bracketed comments and add them to the footnote
+    $str = preg_replace_callback( '|\((.+)\)|', array($this, 'replace_footnote'), $str);
+
+    $this->credit = $this->mytrim($str);
+  }
+
+  private function replace_footnote($matches) {
+    $this->footnote .= $matches[1] . "\n";
+    return "";
   }
 
   private function mytrim($str, $more='') {
   // Because &nbsp; is annoying
-    return trim($str, "$more \t\r\n\0\x0B\xA0");
+    return trim($str, "$more .\t\r\n\0\x0B\xA0");
   }
 }
 
--- test.php
+++ test.php
@@ -4,14 +4,16 @@ require('Campanoscraper/load.php');
 $c = Campanophile::getInstance();
 $db = new Database('localhost', 'root', '', 'campanophile');
 
-$r = $c->search(array('StartDate' => '01/06/2010', 'FinalDate' => '30/06/2010', 'Guild' => 'University of London Society'));
+//$r = $c->search(array('StartDate' => '01/06/2010', 'FinalDate' => '30/06/2010', 'Guild' => 'University of London Society'));
 //var_dump($r);
-$r = current($r);
-$r->fetch_campanophile_details();
+//$r = current($r);
+//$r->fetch_campanophile_details();
+
+$r = $c->get_performance(103998);
 
 print_r($r);
 $r->save();
-print_r($r);
+//print_r($r);
 
 /* testing search_all */
 //$a = $c->search_all(array('StartDate' => '01/01/2009', 'FinalDate' => '31/12/2009', 'Guild' => 'Surrey Association'));
